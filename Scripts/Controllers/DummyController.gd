723ba937fe40ef2942f37fd8ba455fc0634c3a25
VPJP <vpjpeltonen@gmail.com>
2020-10-13 14:21:09 +0300

    ai shoots

--- Scripts/Controllers/DummyController.gd
+++ Scripts/Controllers/DummyController.gd
@@ -1,26 +1,69 @@
 extends KinematicBody
 
+export(Resource) var bullet
+
 export var speed = 5.0
+
 var path = []
 var state = "default"
 var current_target
 var current_node = 0
+
+var spot_range = 25.0
+var shoot_range = 100.0
+var reloaded = true
 
 var model
 var rng = RandomNumberGenerator.new()
 
 onready var nav = get_parent()
 onready var walk_point = [get_parent().get_node("Position3D"),get_parent().get_node("Position3D2"),get_parent().get_node("Position3D3"),get_parent().get_node("Position3D4")]
-	
+onready var Player = get_tree().get_nodes_in_group("Player")[0]
+onready var bullet_source = $BulletSource
 	
 func _ready():
 	rng.randomize()
 	
-func _process(delta):
-	move()
+func _physics_process(delta):
+	match state:
+		"default":
+			look_for_player()
+			move()
+		"shoot":
+			shoot()
+			look_at(Player.global_transform.origin,Vector3.UP)
+			stay_in_range()
+			
+func stay_in_range():
+	var distance = global_transform.origin.distance_to(Player.global_transform.origin) 
+	if distance > spot_range:
+		state = "default"
+
+func look_for_player():
+	var distance = global_transform.origin.distance_to(Player.global_transform.origin) 
+	if distance < spot_range:
+		var space_state = get_world().direct_space_state
+		var result = space_state.intersect_ray(global_transform.origin, Player.global_transform.origin, [self])
+		if result.collider.is_in_group("Player"):
+			state = "shoot"
+
+func shoot():
+	if !reloaded:
+		return
+	var clone = bullet.instance()
+	var scene_root = get_parent()
+	scene_root.add_child(clone)
+	print("shoot")
+	clone.global_transform = bullet_source.global_transform
+	clone.damage = 1
+	clone.speed = 100
+	$ReloadTimer.start()
+	reloaded = false
+
 
 func move():
 	if current_node < path.size():
+		look_at(path[current_node],Vector3.UP)
 		var dir = (path[current_node] - global_transform.origin)
 		if dir.length() < 1:
 			current_node += 1
@@ -36,3 +79,7 @@ func init(worker_model):
 func move_to(target):
 	path = nav.get_simple_path(global_transform.origin, target)
 	current_node = 0
+
+
+func _on_ReloadTimer_timeout():
+	reloaded = true
