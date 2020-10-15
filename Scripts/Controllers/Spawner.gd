f5377d9f94c74e07fcaf4620759e7a10ebb017f9
VPJP <vpjpeltonen@gmail.com>
2020-10-15 16:07:27 +0300

    drone to spawner

--- Scripts/Controllers/Spawner.gd
+++ Scripts/Controllers/Spawner.gd
@@ -3,6 +3,9 @@ extends Spatial
 onready var spawn_points = get_children()
 
 export(Resource) var enemy1
+export(Resource) var drone
+
+onready var basic_enemies = [enemy1,drone]
 
 var rng = RandomNumberGenerator.new()
 
@@ -10,7 +13,7 @@ func _on_Director_spawn_enemies(amount):
 	print("spawning")
 	for i in range(0,amount):
 		var point = rng.randi_range(0,spawn_points.size()-1)
-		var clone = enemy1.instance()
+		var clone = basic_enemies[rng.randi_range(0,basic_enemies.size()-1)].instance()#enemy1.instance()
 		var nav = get_parent()
 		nav.add_child(clone)
 		#print("shoot")
