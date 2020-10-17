950591b058a1b2e86652cc33526c80a128d8ad76
VPJP <vpjpeltonen@gmail.com>
2020-10-17 17:56:25 +0300

    new upgrade UI

--- Scripts/Controllers/RoundPassScreen.gd
+++ Scripts/Controllers/RoundPassScreen.gd
@@ -1,28 +1,54 @@
-extends VBoxContainer
+extends Control
 
 signal pick_done(pick)
+
+export(Texture) var speed_logo
+export(Texture) var health_logo
+export(Texture) var time_logo
+export(Texture) var jump_logo
+export(Texture) var regen_logo
+onready var power_logos = [speed_logo,health_logo,time_logo,jump_logo,regen_logo]
 
 var improvements_pool = ["Speed","Health","Time Manipulation","Jump","Health Regenaration"]
 var stored_improvements = ["Speed","Health","Time Manipulation"]
 
 var rng = RandomNumberGenerator.new()
 
+var displaying = false
+
+func _process(delta):
+	if !displaying:
+		return
+	if Input.is_action_just_pressed("pick_1"):
+		pick_improvement(stored_improvements[0])
+	if Input.is_action_just_pressed("pick_2"):
+		pick_improvement(stored_improvements[1])
+	if Input.is_action_just_pressed("pick_3"):
+		pick_improvement(stored_improvements[2])
+
 func pick_improvement(improvement):
 	emit_signal("pick_done",improvement)
-	Mouse.capture_mouse()
+	displaying = false
 	hide()
 
 func display_improvements():
 	show()
+	displaying = true
+	var logos = []
 	for i in range (0,3):
 		while true:
-			var improvement = improvements_pool[rng.randi_range(0,improvements_pool.size()-1)] 
+			var num = rng.randi_range(0,improvements_pool.size()-1)
+			var improvement = improvements_pool[num] 
 			if !stored_improvements.has(improvement):
 				stored_improvements[i] = improvement
+				logos.append(num)
 				break
-	$Choises/Option1/Label.text = stored_improvements[0]
-	$Choises/Option2/Label.text = stored_improvements[1]
-	$Choises/Option3/Label.text = stored_improvements[2]
+	$Control/Choises/Option1/Label.text = stored_improvements[0]
+	$Control/Choises/Option1/Logo.texture = power_logos[logos[0]]
+	$Control/Choises/Option2/Label.text = stored_improvements[1]
+	$Control/Choises/Option2/Logo.texture = power_logos[logos[1]]
+	$Control/Choises/Option3/Label.text = stored_improvements[2]
+	$Control/Choises/Option3/Logo.texture = power_logos[logos[2]]
 
 func _on_Option1_pressed():
 	pick_improvement(stored_improvements[0])
@@ -35,4 +61,4 @@ func _on_Option3_pressed():
 
 func _on_Director_display_round_done():
 	display_improvements()
-	Mouse.free_mouse(false)
+	#Mouse.free_mouse(false)
