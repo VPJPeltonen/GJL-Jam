509189a2e1a46a5f733a1bde005b18748a93bd45
VPJP <vpjpeltonen@gmail.com>
2020-10-18 18:49:03 +0300

    reduced enemy amount

--- Scripts/Controllers/Spawner.gd
+++ Scripts/Controllers/Spawner.gd
@@ -7,12 +7,13 @@ var rng = RandomNumberGenerator.new()
 var spawn_amount = 0
 
 onready var spawn_points = $Points.get_children()
+onready var spawn_points_drone = $DronePoints.get_children()
 onready var drone_nav = get_parent().get_node("DroneNavigation")
 onready var nav = get_parent().get_node("Navigation")
 
 onready var basic_enemies = [enemy1,drone]
 onready var navs = [nav,drone_nav]
-
+onready var spawns = [spawn_points,spawn_points_drone]
 
 func _on_Director_spawn_enemies(amount):
 	spawn_amount += amount
@@ -26,7 +27,7 @@ func _on_Timer_timeout():
 	var spawnable = rng.randi_range(0,basic_enemies.size()-1)
 	var clone = basic_enemies[spawnable].instance()
 	navs[spawnable].add_child(clone)
-	clone.global_transform = spawn_points[point].global_transform
+	clone.global_transform = spawns[spawnable][point].global_transform
 	if spawn_amount > 0:
 		$Timer.start()
 		spawn_amount -= 1
