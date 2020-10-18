52b9e3f0055d39c0db1dbb7e5a497a3cb6ad8152
LuisMint <62115750+LuisMint@users.noreply.github.com>
2020-10-18 08:08:35 -0700

    sss
    ss

--- Scripts/Controllers/Shots.gd
+++ Scripts/Controllers/Shots.gd
@@ -1,7 +1,7 @@
 extends Area
 
 func _ready():
-	$AnimationPlayer.play("Shot_hover")
+	$Spatial/AnimationPlayer.play("Shot_hover")
 
 func _on_Shots_body_entered(body):
 	if body.is_in_group("Player"):
